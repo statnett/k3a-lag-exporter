3b15cfb8e0198c587706120535c2482583eedbff
Sverre H. Huseby <shh@thathost.com>
2023-09-28 12:16:56 +0200

    feat: Show simple help if any args are given

--- src/main/java/no/statnett/k3alagexporter/K3aLagExporter.java
+++ src/main/java/no/statnett/k3alagexporter/K3aLagExporter.java
@@ -22,7 +22,16 @@ public final class K3aLagExporter {
         }
     }
 
+    private static void showHelpAndExit() {
+        System.out.println("For configuration and usage of k3a-lag-exporter, please see");
+        System.out.println("https://github.com/statnett/k3a-lag-exporter");
+        System.exit(0);
+    }
+
     public static void main(final String[] args) {
+        if (args.length != 0) {
+            showHelpAndExit();
+        }
         new K3aLagExporter().doit();
     }
 
