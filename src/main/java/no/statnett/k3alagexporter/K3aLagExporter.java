866e0a198c9ce49c3313c62e7d2de2079c5b8654
Sverre H. Huseby <shh@thathost.com>
2023-09-28 11:53:02 +0200

    chore: Remove unused method

--- src/main/java/no/statnett/k3alagexporter/K3aLagExporter.java
+++ src/main/java/no/statnett/k3alagexporter/K3aLagExporter.java
@@ -1,14 +1,8 @@
 package no.statnett.k3alagexporter;
 
 import no.statnett.k3alagexporter.model.ClusterData;
-import no.statnett.k3alagexporter.model.ConsumerGroupData;
-import no.statnett.k3alagexporter.model.TopicPartitionData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class K3aLagExporter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(K3aLagExporter.class);
 
     private void doit() {
         final long msBetweenCollections = Conf.getPollIntervalMs();
@@ -26,20 +20,6 @@ public final class K3aLagExporter {
                 throw new RuntimeException(e);
             }
         }
-    }
-
-    private void dumpToStdout(final ClusterData clusterData) {
-        int numTopicPartitions = 0;
-        int numConsumerGroups = 0;
-        for (final TopicPartitionData topicPartitionData : clusterData.getAllTopicPartitionData()) {
-            ++ numTopicPartitions;
-            System.out.println("Partition: " + topicPartitionData.getTopicPartition() + ": endOffset=" + topicPartitionData.getEndOffset());
-            for (final ConsumerGroupData consumerGroupData : topicPartitionData.getConsumerGroupDataMap().values()) {
-                ++numConsumerGroups;
-                System.out.println("  Consumer group: " + consumerGroupData.getConsumerGroupId() + ": offset=" + consumerGroupData.getOffset() + ", lag=" + consumerGroupData.getLag());
-            }
-        }
-        System.out.println("\nIn total " + numTopicPartitions + " topic partitions and " + numConsumerGroups + " consumer groups.");
     }
 
     public static void main(final String[] args) {
