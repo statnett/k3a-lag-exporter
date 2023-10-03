0afc51dc9657b4bb62c9e506cd9ea17fb2cbbf24
Sverre H. Huseby <shh+github@thathost.com>
2023-10-03 11:00:46 +0200

    Update src/main/java/no/statnett/k3alagexporter/utils/LogUtils.java
    Co-authored-by: Erik Godding Boye <egboye@gmail.com>

--- src/main/java/no/statnett/k3alagexporter/utils/LogUtils.java
+++ src/main/java/no/statnett/k3alagexporter/utils/LogUtils.java
@@ -11,7 +11,7 @@ public final class LogUtils {
      * Sets up logging to a single line when using Java Logging.
      * Must be called before any log-related class is instantiated.
      */
-    public static void enableSingleLineLogging() {
+    public static void initLogging() {
         if (System.getProperty(LOG_FORMAT_PROPERTY) == null) {
             System.setProperty(LOG_FORMAT_PROPERTY, "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s: %5$s%6$s%n");
         }
