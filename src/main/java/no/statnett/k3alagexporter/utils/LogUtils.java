d8d4dfc15e1e197d5b672db14825383d83105254
Sverre H. Huseby <shh@thathost.com>
2023-10-03 07:27:11 +0200

    Add colon

--- src/main/java/no/statnett/k3alagexporter/utils/LogUtils.java
+++ src/main/java/no/statnett/k3alagexporter/utils/LogUtils.java
@@ -11,6 +11,6 @@ public final class LogUtils {
      */
     public static void enableSingleLineLogging() {
         System.setProperty("java.util.logging.SimpleFormatter.format",
-                           "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
+                           "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s: %5$s%6$s%n");
     }
 }
